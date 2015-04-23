1351b48c2ed21d47061888ed07f2cdc56d5b6a03
kc <kcthota@gmail.com>
2015-04-23 01:28:14 -0700

    added orExpression

---
+++ src/main/java/com/kcthota/JSONQuery/expressions/OrExpression.java
@@ -0,0 +1,9 @@
+package com.kcthota.JSONQuery.expressions;
+
+public class OrExpression extends AbstractMultiExpression {
+
+	public OrExpression(Expression... expressions) {
+		super(expressions);
+	}
+
+}
