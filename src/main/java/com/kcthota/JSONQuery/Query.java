570667cd364bc16813d46b3aa8f4ee56031c39a7
kc <kcthota@gmail.com>
2015-04-23 01:40:32 -0700

    handle exceptions

--- src/main/java/com/kcthota/JSONQuery/Query.java
+++ src/main/java/com/kcthota/JSONQuery/Query.java
@@ -12,53 +12,71 @@ import com.kcthota.JSONQuery.expressions.NeExpression;
 import com.kcthota.JSONQuery.expressions.OrExpression;
 
 public class Query {
-	
+
 	private JsonNode node;
-	
+
 	public Query(JsonNode node) {
 		this.node = node;
 	}
-	
+
 	public boolean is(Expression expr) {
 		return is(expr, null);
 	}
-	
+
 	private boolean is(Expression expr, Boolean currentResult) {
-		//at any time if currentResult is false, stop and return false;
-		if(currentResult!=null && !currentResult)
+		// at any time if currentResult is false, stop and return false;
+		if (currentResult != null && !currentResult)
 			return false;
 
-		if(expr instanceof ComparisonExpression) {
-			ComparisonExpression castedExpr = (ComparisonExpression) expr;
-			if(castedExpr instanceof EqExpression) {
-				return node.get(castedExpr.property()).equals(castedExpr.value());
-			} else if(expr instanceof NeExpression) {
-				return !node.get(castedExpr.property()).equals(castedExpr.value());
-			}
-		} else if(expr instanceof MultiExpression) {
-			MultiExpression castedExpr = (MultiExpression) expr;
-			List<Expression> expressions = castedExpr.getExpressions();
-			if(castedExpr instanceof AndExpression) {
-				for(Expression curExpr : expressions) {
-					currentResult = is(curExpr,currentResult);
-					if(!currentResult) {
-						return false;
-					}
+		try {
+			if (expr instanceof ComparisonExpression) {
+				ComparisonExpression castedExpr = (ComparisonExpression) expr;
+				if (castedExpr instanceof EqExpression) {
+					return getValue(node, castedExpr.property()).equals(castedExpr.value());
+				} else if (expr instanceof NeExpression) {
+					return !getValue(node, castedExpr.property()).equals(castedExpr.value());
 				}
-				return true;
-			} else if(castedExpr instanceof OrExpression) {
-				for(Expression curExpr : expressions) {
-					currentResult = is(curExpr,currentResult);
-					if(currentResult) {
-						return true;
+			} else if (expr instanceof MultiExpression) {
+				MultiExpression castedExpr = (MultiExpression) expr;
+				List<Expression> expressions = castedExpr.getExpressions();
+				if (castedExpr instanceof AndExpression) {
+					for (Expression curExpr : expressions) {
+						currentResult = is(curExpr, currentResult);
+						if (!currentResult) {
+							return false;
+						}
 					}
-					
-					//reset currentResult for Or
-					currentResult=null;
+					return true;
+				} else if (castedExpr instanceof OrExpression) {
+					for (Expression curExpr : expressions) {
+						currentResult = is(curExpr, currentResult);
+						if (currentResult) {
+							return true;
+						}
+
+						// reset currentResult for Or
+						currentResult = null;
+					}
+					return false;
 				}
-				return false;
 			}
+		} catch (Exception e) {
+			//ignore exception and return false for now.
+			//TODO handle exceptions
 		}
 		return false;
+	}
+
+	private JsonNode getValue(JsonNode node, String property){
+		JsonNode value=null;
+		
+		if(property.indexOf('.')<0) {
+			value = node.get(property);
+			if(value==null) {
+				throw new RuntimeException("Property doesn't exist: "+property);
+			}
+			return value;
+		}
+		throw new RuntimeException("hierarchical paths not supported");
 	}
 }
