60a2c970c43d7a5450d153d889d69c2b77d320b7
kc <kcthota@gmail.com>
2015-05-08 20:17:37 -0700

    return proper expression types

--- src/main/java/com/kcthota/JSONQuery/expressions/Expr.java
+++ src/main/java/com/kcthota/JSONQuery/expressions/Expr.java
@@ -15,163 +15,163 @@ import com.fasterxml.jackson.databind.node.TextNode;
  */
 public class Expr {
 	
-	public static ComparisonExpression eq(String property, String value) {
+	public static EqExpression eq(String property, String value) {
 		return eq(val(property), TextNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression eq(String property, Integer value) {
+	public static EqExpression eq(String property, Integer value) {
 		return eq(val(property), IntNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression eq(String property, Long value) {
+	public static EqExpression eq(String property, Long value) {
 		return eq(val(property), LongNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression eq(String property, boolean value) {
+	public static EqExpression eq(String property, boolean value) {
 		return eq(val(property), BooleanNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression eq(String property, JsonNode value) {
+	public static EqExpression eq(String property, JsonNode value) {
 		return eq(val(property), value);
 	}
 	
-	public static ComparisonExpression eq(ValueExpression expression, String value) {
+	public static EqExpression eq(ValueExpression expression, String value) {
 		return eq(expression, TextNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression eq(ValueExpression expression, JsonNode value) {
+	public static EqExpression eq(ValueExpression expression, JsonNode value) {
 		return new EqExpression(expression, value);
 	}
 	
-	public static ComparisonExpression ne(String property, String value) {
+	public static NeExpression ne(String property, String value) {
 		return ne(val(property), TextNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression ne(String property, Integer value) {
+	public static NeExpression ne(String property, Integer value) {
 		return ne(val(property), IntNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression ne(String property, Long value) {
+	public static NeExpression ne(String property, Long value) {
 		return ne(val(property), LongNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression ne(String property, boolean value) {
+	public static NeExpression ne(String property, boolean value) {
 		return ne(val(property), BooleanNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression ne(String property, JsonNode value) {
+	public static NeExpression ne(String property, JsonNode value) {
 		return ne(val(property), value);
 	}
 	
-	public static ComparisonExpression ne(ValueExpression expression, String value) {
+	public static NeExpression ne(ValueExpression expression, String value) {
 		return ne(expression, TextNode.valueOf(value));
 	}
 
-	public static ComparisonExpression ne(ValueExpression expression, JsonNode value) {
+	public static NeExpression ne(ValueExpression expression, JsonNode value) {
 		return new NeExpression(expression, value);
 	}
 	
-	public static ComparisonExpression gt(String property, Integer value) {
+	public static GtExpression gt(String property, Integer value) {
 		return new GtExpression(val(property), IntNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression gt(String property, Long value) {
+	public static GtExpression gt(String property, Long value) {
 		return new GtExpression(val(property), LongNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression gt(String property, Float value) {
+	public static GtExpression gt(String property, Float value) {
 		return new GtExpression(val(property), FloatNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression gt(String property, Double value) {
+	public static GtExpression gt(String property, Double value) {
 		return new GtExpression(val(property), DoubleNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression lt(String property, Integer value) {
+	public static LtExpression lt(String property, Integer value) {
 		return new LtExpression(val(property), IntNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression lt(String property, Long value) {
+	public static LtExpression lt(String property, Long value) {
 		return new LtExpression(val(property), LongNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression lt(String property, Float value) {
+	public static LtExpression lt(String property, Float value) {
 		return new LtExpression(val(property), FloatNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression lt(String property, Double value) {
+	public static LtExpression lt(String property, Double value) {
 		return new LtExpression(val(property), DoubleNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression ge(String property, Integer value) {
+	public static GeExpression ge(String property, Integer value) {
 		return new GeExpression(val(property), IntNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression ge(String property, Long value) {
+	public static GeExpression ge(String property, Long value) {
 		return new GeExpression(val(property), LongNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression ge(String property, Float value) {
+	public static GeExpression ge(String property, Float value) {
 		return new GeExpression(val(property), FloatNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression ge(String property, Double value) {
+	public static GeExpression ge(String property, Double value) {
 		return new GeExpression(val(property), DoubleNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression le(String property, Integer value) {
+	public static LeExpression le(String property, Integer value) {
 		return new LeExpression(val(property), IntNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression le(String property, Long value) {
+	public static LeExpression le(String property, Long value) {
 		return new LeExpression(val(property), LongNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression le(String property, Float value) {
+	public static LeExpression le(String property, Float value) {
 		return new LeExpression(val(property), FloatNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression le(String property, Double value) {
+	public static LeExpression le(String property, Double value) {
 		return new LeExpression(val(property), DoubleNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression and(ComparisonExpression...expressions) {
+	public static AndExpression and(ComparisonExpression...expressions) {
 		return new AndExpression(expressions);
 	}
 	
-	public static ComparisonExpression or(ComparisonExpression...expressions) {
+	public static OrExpression or(ComparisonExpression...expressions) {
 		return new OrExpression(expressions);
 	}
 	
-	public static ComparisonExpression Null(String property) {
+	public static IsNullExpression Null(String property) {
 		return new IsNullExpression(val(property));
 	}
 	
-	public static ComparisonExpression not(ComparisonExpression expr) {
+	public static NotExpression not(ComparisonExpression expr) {
 		return new NotExpression(expr);
 	}
 	
-	public static ComparisonExpression substringof(String property, String value) {
+	public static SubstringOfExpression substringof(String property, String value) {
 		return substringof(val(property), value);
 	}
 	
-	public static ComparisonExpression substringof(ValueExpression expression, String value) {
+	public static SubstringOfExpression substringof(ValueExpression expression, String value) {
 		return new SubstringOfExpression(expression, TextNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression startsWith(String property, String value) {
+	public static StartsWithExpression startsWith(String property, String value) {
 		return startsWith(val(property), value);
 	}
 	
-	public static ComparisonExpression startsWith(ValueExpression expression, String value) {
+	public static StartsWithExpression startsWith(ValueExpression expression, String value) {
 		return new StartsWithExpression(expression, TextNode.valueOf(value));
 	}
 	
-	public static ComparisonExpression endsWith(String property, String value) {
+	public static EndsWithExpression endsWith(String property, String value) {
 		return endsWith(val(property), value);
 	}
 	
-	public static ComparisonExpression endsWith(ValueExpression expression, String value) {
+	public static EndsWithExpression endsWith(ValueExpression expression, String value) {
 		return new EndsWithExpression(expression, TextNode.valueOf(value));
 	}
 	
